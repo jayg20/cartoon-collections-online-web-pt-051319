adc89c5836ad102f52728fb1b99248eccc90dfc3
James Gomez <james.gomez320@yahoo.com>
2019-05-24 02:35:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name, index|
+    return puts "#{index+1} #{name}"
+   end
+ end 
+    
+  
+  
 end
 
 def summon_captain_planet# code an argument here
